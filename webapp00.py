0a30e12cec76ae020547c2408e671d2a6feac687
Yokono004 <113910131+Yokono004@users.noreply.github.com>
2022-09-19 10:28:30 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,13 +4,13 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Meu prof. é Incrível!")
+st.title("Programer")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Cabeçalho")
+st.header("MackWeek")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
+st.subheader("Curso de programação")
 
 # Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.write("Primeiros passos, conceitos básicos de phyton e anotações")
